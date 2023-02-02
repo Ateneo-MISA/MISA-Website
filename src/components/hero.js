4f9ca1a6486f3ea51c7bf3c96b6d62aea6c509fe
Marco Francisco <marcofrancisco@Marcos-MacBook-Pro.local>
2023-02-02 21:49:30 +0800

    home page(feature): fix wrong font

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import { GatsbyImage } from 'gatsby-plugin-image'
 import { StaticImage } from 'gatsby-plugin-image'
-import { renderRichText } from 'gatsby-source-contentful/rich-text'
 
 import * as styles from './hero.module.css'
 
 const Hero = () => (
-  <div className={`${styles.hero}`}>
+  <div className={`${styles.hero} font-abc`}>
     <div className={`flex flex-col md:flex-row lg:flex-row pt-20 lg:ml-20 pb-20 lg:pb-10`}>
       <div className='flex-initial justify-center'>
         <StaticImage
           className={`${styles.image} lg:mx-20`}
-          src="../../static/images/misa-bot.png"
+          src="../../static/images/b.png"
         />
       </div>
       <div className={`${styles.heroContent} flex-1 text-center md:text-left lg:text-left lg:w-1/2 mx-5`}>
         <p className="italic text-lg md:text-xl lg:text-xl">
           Ateneo Management Information Systems Association
         </p>
-        <h2 className="text-4xl lg:text-5xl font-extrabold">
+        <h2 className="text-4xl lg:text-5xl font-extrabold w-full lg:w-4/5">
           We build tomorrow through Business Technology
         </h2>
         <button
