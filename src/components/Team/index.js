bfe2fb563cc1645065c5edc2a53df5b6e8dcfc51
MarcoFrancisco13 <franciscomarco2921@gmail.com>
2023-06-04 19:19:16 +0800

    commit to upstream

---
+++ src/components/Team/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../Layout/index'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const TeamPage = () => {
+    return (
+        <Layout>
+            <h1> elp</h1>
+        </Layout>
+    )
+}
+
+export default TeamPage
