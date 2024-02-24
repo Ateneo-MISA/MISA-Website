518f681eb68cb9f39261d2d61f7234778e11815c
Jericho Arcelao <jerichoarcelao@gmail.com>
2024-02-24 09:33:09 +0000

    remove staging env

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host: process.env.CONTENTFUL_HOST,
-        environment: environment === 'production' ? 'master' : 'staging',
+        environment: 'master',
       },
     },
   ],
