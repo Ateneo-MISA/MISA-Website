d5b0eda621d37438cdf28f542fdf4db79d077a2b
Jericho Arcelao <jerichoarcelao@gmail.com>
2024-02-24 10:08:59 +0000

    temp disable home carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import React from 'react'
 import Layout from '../components/Layout/index'
 import Hero from '../components/Home/Hero'
 import UpcomingEvents from '../components/Home/UpcomingEvents'
-import HomeCarousel from '../components/Home/HomeCarousel'
+// import HomeCarousel from '../components/Home/HomeCarousel'
 
 class RootIndex extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
         <Hero />
-        <HomeCarousel />
+        {/* <HomeCarousel /> */}
         <UpcomingEvents />
       </Layout>
     )
